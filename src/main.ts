1c97d77800010fa5ac29c779ca2cd3aa4f42235a
Amaël Kesteman <amael.kesteman@etudiant.univ-rennes1.fr>
2025-01-02 16:32:46 +0100

    modification du main pour composant standalone

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 import { AppModule } from './app/app.module';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true
-})
+// Démarrage de l'application avec AppModule
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+// Démarrage du composant standalone AppComponent
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideAnimationsAsync(),
+  ]
+})
+  .catch((err) => console.error(err));
